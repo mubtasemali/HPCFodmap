12225145144039f8ceded79aa30bac9e44137cc6
Ben Weidner <Weidnerb32@outlook.com>
2024-03-22 13:59:01 -0600

    Removed Food Issues

---
+++ HPCFodmapProject/Server/Data/Migrations/20240322195740_Remove_Issues_From_Food_Table.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HPCFodmapProject.Server.Data.Migrations
+{
+    public partial class Remove_Issues_From_Food_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "issues",
+                table: "Food");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "issues",
+                table: "Food",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
