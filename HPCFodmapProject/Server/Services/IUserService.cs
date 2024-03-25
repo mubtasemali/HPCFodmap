bcc3ff4bb7bc0130c91c3d1d5ac99c4a9e07a93a
mubtasemali <mubtasemali36@outlook.com>
2024-03-25 12:18:36 -0600

    fixed interface for services

--- HPCFodmapProject/Server/Services/IUserService.cs
+++ HPCFodmapProject/Server/Services/IUserService.cs
@@ -2,6 +2,8 @@
 {
     public interface IUserService
     {
+        Task<bool> IsFlaggedFood(string username, int foodId);
+        Task<bool> IsWhiteList(string username, int ingredientsId);
 
     }
 }
