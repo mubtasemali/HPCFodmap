0cb26f2659f2c3dc027bd230eb6f451552e33e31
Ben Weidner <Weidnerb32@outlook.com>
2024-03-25 22:33:27 -0600

    I have added first and last name entries when creating an account.

--- HPCFodmapProject/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ HPCFodmapProject/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,6 +80,8 @@ namespace HPCFodmapProject.Server.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
+            public string firstname { get; set; }
+            public string lastname { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -114,6 +116,8 @@ namespace HPCFodmapProject.Server.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+                user.firstname = Input.firstname;
+                user.lastname = Input.lastname;
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
