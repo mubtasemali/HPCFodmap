7ba5b1ec23986b1a19d800aa276d91712eb36044
Michael Yap <969063@cognizant.com>
2024-03-26 14:31:10 -0600

    Created new page for Flagged/Whitelisted Ingredients

---
+++ HPCFodmapProject/Client/Pages/Flagged.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+using HPCFodmapProject.Shared;
+using System.Net.Http.Json;
+
+
+namespace HPCFodmapProject.Client.Pages;
+
+
+
+public partial class Flagged
+{
+    [Inject]
+    AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+    [Inject]
+    HttpClient Http { get; set; }
+    List<IntakeDto> foodDiary = new List<IntakeDto>();
+    protected override async Task OnInitializedAsync()
+
+    {
+
+        var UserAuth = (await AuthenticationStateProvider.GetAuthenticationStateAsync()).User.Identity;
+        if (UserAuth is not null && UserAuth.IsAuthenticated)
+        {
+         
+            //IntakeDto intakeDto = await Http.GetFromJsonAsync<IntakeDto>("api/getUserFoodIntak?username=" + UserAuth.Name);
+            foodDiary = await Http.GetFromJsonAsync<List<IntakeDto>>("api/getUserFoodIntake?username=" + UserAuth.Name);
+            //var foodDiary = await HttpClient.GetAsync<List<IntakeDto>>("/api/cars");
+            //if (foodDiary?.Any() ?? false)
+            //{
+            //    foreach(var food in foodDiary)
+            //    {
+            //        IntakeDto intake = await Http.GetFromJsonAsync<IntakeDto>(food.ToString());
+            //    }
+        }
+    }
+}
+    
