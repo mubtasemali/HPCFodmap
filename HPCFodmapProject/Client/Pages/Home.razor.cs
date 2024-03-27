31acad12b8f51e99ff2dc85e2bd7b7ce4871422b
Ben Weidner <Weidnerb32@outlook.com>
2024-03-27 15:45:45 -0600

    Updated changes working on home

--- HPCFodmapProject/Client/Pages/Home.razor.cs
+++ HPCFodmapProject/Client/Pages/Home.razor.cs
@@ -22,10 +22,10 @@ public partial class Home
     [Inject]
     HttpClient Http { get; set; }
     List<IntakeDto> foodDiary = new List<IntakeDto>();
-    //IntakeDto newEntry = new IntakeDto();
     //NEW VARIABLES FOR AddFoodIntake
     public string food = null;
     public string notes = null;
+    //This user variable is to pass to the AddFoodIntake method
     public string userN = null;
     protected override async Task OnInitializedAsync()
 
@@ -41,39 +41,28 @@ public partial class Home
             //foodDiary = await Http.GetFromJsonAsync<List<IntakeDto>>("api/getUserFoodIntake?username=" + UserAuth.Name);
 
         }
-
-        //trying something
-        //var testing = AddFoodIntake(uName, food, notes);
        
     }
-    //adding new method Method gets user input values and assigns them to variables. 
+    //adding new method Method gets user input for food name and food notes from the front end after the button is clicked 
    public void getUserInputForEntry(string foodI, string notesI)
     {
-        Console.WriteLine("Passed food sucessfully: " + foodI);
-        Console.WriteLine("Passed note sucessfully: " + notesI);
-      
-
-
-     
+        //console writelines for testing
+        //Console.WriteLine("Passed food sucessfully: " + foodI);
+        //Console.WriteLine("Passed note sucessfully: " + notesI);
+     //calls the food result method which connexts to the controller api route so that we can pass in user
         var foodResult = AddFoodIntake(userN, foodI, notesI);
         Console.WriteLine("food result: " + foodResult);
     }
-    //adding method temporarily 
+    // 
     public async Task<bool> AddFoodIntake(string userName, string foodName, string notes)
     {
-        //making sure it is being passed into method
-        Console.WriteLine("Passed food sucessfully into second method: " + foodName);
-        Console.WriteLine("Passed note sucessfully into second method: " + notes);
-        Console.WriteLine("Passed username sucessfully into second method: " + userName);
-        //IntakeDto entry = new IntakeDto();
-        //returning a 404
+        //making sure it is being passed into method testing
+        //Console.WriteLine("Passed food sucessfully into second method: " + foodName);
+        //Console.WriteLine("Passed note sucessfully into second method: " + notes);
+        //Console.WriteLine("Passed username sucessfully into second method: " + userName);
         string passString = $"api/addfoodintake?username={userName}&foodname={foodName}&notes={notes}";
-      
-
-     
+     //do not need
         //string newString = HttpUtility.UrlEncode(passString);
-
-
         var res = await Http.GetFromJsonAsync<bool>(passString);
         //var res = await Http.PostAsJsonAsync($"api/addfoodintake?username={userName}/foodname={foodName}/notes={notes}", entry);
         if (res)
