676acb18cce9ccf8ac9674f5bd3fa48e778c450f
Ben Weidner <Weidnerb32@outlook.com>
2024-03-26 18:33:03 -0600

    Resolved Issue we struggled on. Now trying to resolve another issue.

---
+++ HPCFodmapProject/Client/HttpRepository/UserFoodDiaryHttpRepository.cs
@@ -0,0 +1,28 @@
+﻿using HPCFodmapProject.Shared;
+using System.Net.Http.Json;
+
+
+namespace HPCFodmapProject.Client.HttpRepository;
+
+public class UserFoodDiaryHttpRepository
+{
+  
+
+        //HttpClient Http { get; set; }
+        private readonly HttpClient _httpClient;
+        public async Task<List<IntakeDto>> GetIngredients(string userName)
+        {
+            List<IntakeDto>? foodDiary = new List<IntakeDto>();
+        //says problem with below 
+            foodDiary = await _httpClient.GetFromJsonAsync<List<IntakeDto>>("api/getUserFoodIntake?username=" + userName);
+            //if (foodDiary == null)
+            //{
+               
+            //}
+            return foodDiary;
+
+
+        }
+       
+    
+}
