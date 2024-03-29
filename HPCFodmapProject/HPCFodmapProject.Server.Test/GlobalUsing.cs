16ca9a9f8bf764d73285cfb1cbc2d28119ca99df
mubtasemali <mubtasemali36@outlook.com>
2024-03-28 19:19:03 -0600

    New stuff

---
+++ HPCFodmapProject/HPCFodmapProject.Server.Test/GlobalUsing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCFodmapProject.Client.Test
+{
+    internal class GlobalUsing
+    {
+    }
+}
