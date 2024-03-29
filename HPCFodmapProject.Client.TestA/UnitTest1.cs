42bb45057465256bdb33c09c5aef1915749f8f28
mubtasemali <mubtasemali36@outlook.com>
2024-03-29 12:15:58 -0600

    updated depenecies in test projects

--- HPCFodmapProject.Client.TestA/UnitTest1.cs
+++ HPCFodmapProject.Client.TestA/UnitTest1.cs
@@ -1,3 +1,6 @@
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
 namespace HPCFodmapProject.Client.TestA
 {
     public class Tests
@@ -8,9 +11,27 @@ namespace HPCFodmapProject.Client.TestA
         }
 
         [Test]
-        public void Test1()
+        public async Task Test_GetFoodintakeHomePage()
         {
-            Assert.Pass();
+            //arrange
+            var mockHttp = new MockHttpMessageHandler();
+            var testResponse = "\\[{\\\"food\\\":\\\"hotdog\\\",\\\"notes\\\":\\\"bad\\\",\\\"date\\\":\\\"2024-03-25T13:15:47.8650963\\\",\\\"harmful\\\":true},{\\\"food\\\":\\\"hotdog\\\",\\\"notes\\\":\\\"bad\\\",\\\"date\\\":\\\"2024-03-25T13:19:05.3917633\\\",\\\"harmful\\\":true},{\\\"food\\\":\\\"burgers\\\",\\\"notes\\\":\\\"was ok\\\",\\\"date\\\":\\\"2024-03-28T17:21:28.4438481\\\",\\\"harmful\\\":false},{\\\"food\\\":\\\"hotdog\\\",\\\"notes\\\":\\\"was ok\\\",\\\"date\\\":\\\"2024-03-28T17:21:37.2425487\\\",\\\"harmful\\\":true},{\\\"food\\\":\\\"cookies\\\",\\\"notes\\\":\\\"was ok\\\",\\\"date\\\":\\\"2024-03-28T17:21:40.7761023\\\",\\\"harmful\\\":false},{\\\"food\\\":\\\"squash\\\",\\\"notes\\\":\\\"was ok\\\",\\\"date\\\":\\\"2024-03-28T17:21:44.425102\\\",\\\"harmful\\\":false},{\\\"food\\\":\\\"curry\\\",\\\"notes\\\":\\\"was ok\\\",\\\"date\\\":\\\"2024-03-28T17:21:48.7910556\\\",\\\"harmful\\\":false}\\]";
+            //assert
+            mockHttp.When("https://localhost:7192/api/getUserFoodIntake?username=m2@example.com")
+            .Respond("application/json", testResponse);
+            var client = mockHttp.ToHttpClient();
+            client.BaseAddress = new Uri("https://localhost:7176/");
+            var userFoodIntakeRepo = new UserFoodDiaryHttpRepository(client);
+            //Act
+
+
+            var response = await userFoodIntakeRepo.GetIngredients("m2@example.com");
+            var actual = JsonConvert.DeserializeObject<string>(testResponse);
+            var foodData = JsonConvert.DeserializeObject<List<JObject>>(actual);
+            //var actual = response[1].
+            //Assert
+            Assert.That(foodData[0]["food"], Is.EqualTo(response[0].Food));
+
         }
     }
 }
