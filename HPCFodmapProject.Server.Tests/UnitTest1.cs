2e5dffceabd50cc17fecc0c7b010acde7a969a9f
mubtasemali <mubtasemali36@outlook.com>
2024-03-29 11:12:12 -0600

    Added Unit test projects

---
+++ HPCFodmapProject.Server.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace HPCFodmapProject.Server.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
