e9f725e4c39c72845cc5f8be9f44b1495f98e586
mubtasemali <mubtasemali36@outlook.com>
2024-03-29 15:48:03 -0600

    Added Get Tests

--- HPCFodmapProject.Client.TestA/UnitTest1.cs
+++ HPCFodmapProject.Client.TestA/UnitTest1.cs
@@ -1,5 +1,10 @@
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using HPCFodmapProject.Client.HttpRepository;
+using RichardSzalay.MockHttp;
+using NUnit.Framework;
+using System.Collections.Generic;
+using HPCFodmapProject.Shared; // Add this line
 
 namespace HPCFodmapProject.Client.TestA
 {
@@ -8,30 +13,90 @@ namespace HPCFodmapProject.Client.TestA
         [SetUp]
         public void Setup()
         {
+            
         }
 
         [Test]
-        public async Task Test_GetFoodintakeHomePage()
+        public async Task TestGetFoodintakeHomePage()
         {
             //arrange
             var mockHttp = new MockHttpMessageHandler();
-            var testResponse = "\\[{\\\"food\\\":\\\"hotdog\\\",\\\"notes\\\":\\\"bad\\\",\\\"date\\\":\\\"2024-03-25T13:15:47.8650963\\\",\\\"harmful\\\":true},{\\\"food\\\":\\\"hotdog\\\",\\\"notes\\\":\\\"bad\\\",\\\"date\\\":\\\"2024-03-25T13:19:05.3917633\\\",\\\"harmful\\\":true},{\\\"food\\\":\\\"burgers\\\",\\\"notes\\\":\\\"was ok\\\",\\\"date\\\":\\\"2024-03-28T17:21:28.4438481\\\",\\\"harmful\\\":false},{\\\"food\\\":\\\"hotdog\\\",\\\"notes\\\":\\\"was ok\\\",\\\"date\\\":\\\"2024-03-28T17:21:37.2425487\\\",\\\"harmful\\\":true},{\\\"food\\\":\\\"cookies\\\",\\\"notes\\\":\\\"was ok\\\",\\\"date\\\":\\\"2024-03-28T17:21:40.7761023\\\",\\\"harmful\\\":false},{\\\"food\\\":\\\"squash\\\",\\\"notes\\\":\\\"was ok\\\",\\\"date\\\":\\\"2024-03-28T17:21:44.425102\\\",\\\"harmful\\\":false},{\\\"food\\\":\\\"curry\\\",\\\"notes\\\":\\\"was ok\\\",\\\"date\\\":\\\"2024-03-28T17:21:48.7910556\\\",\\\"harmful\\\":false}\\]";
-            //assert
+            var testResponse = @"[{""food"":""hotdog"",""notes"":""bad"",""date"":""2024-03-25T13:15:47.8650963"",""harmful"":true},{""food"":""hotdog"",""notes"":""bad"",""date"":""2024-03-25T13:19:05.3917633"",""harmful"":true},{""food"":""burgers"",""notes"":""was ok"",""date"":""2024-03-28T17:21:28.4438481"",""harmful"":false},{""food"":""hotdog"",""notes"":""was ok"",""date"":""2024-03-28T17:21:37.2425487"",""harmful"":true},{""food"":""cookies"",""notes"":""was ok"",""date"":""2024-03-28T17:21:40.7761023"",""harmful"":false},{""food"":""squash"",""notes"":""was ok"",""date"":""2024-03-28T17:21:44.425102"",""harmful"":false},{""food"":""curry"",""notes"":""was ok"",""date"":""2024-03-28T17:21:48.7910556"",""harmful"":false}]";
+
             mockHttp.When("https://localhost:7192/api/getUserFoodIntake?username=m2@example.com")
-            .Respond("application/json", testResponse);
+                .Respond("application/json", testResponse);
+
             var client = mockHttp.ToHttpClient();
-            client.BaseAddress = new Uri("https://localhost:7176/");
+            client.BaseAddress = new Uri("https://localhost:7192");
+
             var userFoodIntakeRepo = new UserFoodDiaryHttpRepository(client);
+
             //Act
+            var response = await userFoodIntakeRepo.GetFoodIntake("m2@example.com");
 
-
-            var response = await userFoodIntakeRepo.GetIngredients("m2@example.com");
-            var actual = JsonConvert.DeserializeObject<string>(testResponse);
-            var foodData = JsonConvert.DeserializeObject<List<JObject>>(actual);
-            //var actual = response[1].
             //Assert
-            Assert.That(foodData[0]["food"], Is.EqualTo(response[0].Food));
+            var foodData = JsonConvert.DeserializeObject<List<IntakeDto>>(testResponse);
+            Assert.That(response[0].Food, Is.EqualTo(foodData[0].Food));
+            Assert.That(response[1].Food, Is.EqualTo(foodData[1].Food));
+            Assert.That(response[2].Food, Is.EqualTo(foodData[2].Food));
+            Assert.That(response[3].Food, Is.EqualTo(foodData[3].Food));
+            Assert.That(response[4].Food, Is.EqualTo(foodData[4].Food));
+            Assert.That(response[0].harmful, Is.EqualTo(foodData[0].harmful));
+            Assert.That(response[1].harmful, Is.EqualTo(foodData[1].harmful));
+            Assert.That(response[2].harmful, Is.EqualTo(foodData[2].harmful));
+            Assert.That(response[3].harmful, Is.EqualTo(foodData[3].harmful));
+            Assert.That(response[4].harmful, Is.EqualTo(foodData[4].harmful));
+            Assert.That(response[0].date, Is.EqualTo(foodData[0].date));
+            Assert.That(response[1].date, Is.EqualTo(foodData[1].date));
+            Assert.That(response[2].date, Is.EqualTo(foodData[2].date));
+            Assert.That(response[3].date, Is.EqualTo(foodData[3].date));
+            Assert.That(response[4].date, Is.EqualTo(foodData[4].date));
+            Assert.That(response[0].notes, Is.EqualTo(foodData[0].notes));
+            Assert.That(response[1].notes, Is.EqualTo(foodData[1].notes));
+            Assert.That(response[2].notes, Is.EqualTo(foodData[2].notes));
+            Assert.That(response[3].notes, Is.EqualTo(foodData[3].notes));
+            Assert.That(response[4].notes, Is.EqualTo(foodData[4].notes));
+            Assert.That(response.Count, Is.EqualTo(7));
 
         }
-    }
+
+        [Test]
+        public async Task TestGetIngredients()
+        {
+            //arrange
+            var mockHttp = new MockHttpMessageHandler();
+            var testResponse=  @"[{""IngredientsName"":""flour"",""harmful"":true,""inFodMap"":false},{""IngredientsName"":""tomato"",""harmful"":false,""inFodMap"":false},{""IngredientsName"":""yeast"",""harmful"":true,""inFodMap"":false}]"; ;
+            mockHttp.When("https://localhost:7192/api/getIngredients?foodName=burgers&username=m2%40example.com").Respond("application/json", testResponse);
+            var client = mockHttp.ToHttpClient();
+            client.BaseAddress = new Uri("https://localhost:7192");
+            var userFoodIntakeRepo = new UserFoodDiaryHttpRepository(client);
+            //act
+            var response = await userFoodIntakeRepo.GetIngredients("burgers", "m2@example.com");
+            //assert
+            var ingredientData = JsonConvert.DeserializeObject<List<IngredientsDto>>(testResponse);
+            Assert.That(response[0].IngredientsName, Is.EqualTo(ingredientData[0].IngredientsName));
+            Assert.That(response[1].IngredientsName, Is.EqualTo(ingredientData[1].IngredientsName));
+            Assert.That(response[2].IngredientsName, Is.EqualTo(ingredientData[2].IngredientsName));
+            Assert.That(response[0].harmful, Is.EqualTo(ingredientData[0].harmful));
+            Assert.That(response[1].harmful, Is.EqualTo(ingredientData[1].harmful));
+            Assert.That(response[2].harmful, Is.EqualTo(ingredientData[2].harmful));
+            Assert.That(response[0].inFodMap, Is.EqualTo(ingredientData[0].inFodMap));
+            Assert.That(response[1].inFodMap, Is.EqualTo(ingredientData[1].inFodMap));
+            Assert.That(response[2].inFodMap, Is.EqualTo(ingredientData[2].inFodMap));
+            Assert.That(response.Count, Is.EqualTo(3));
+
+
+        }
+
+        public async Task TestGetFoodIntake()
+        {
+               //arrange
+            var mockHttp = new MockHttpMessageHandler();
+            var testResponse = @"[{""food"":""hotdog"",""notes"":""bad"",""date"":""2024-03-25T13:15:47.8650963"",""harmful"":true},{""food"":""hotdog"",""notes"":""bad"",""date"":""2024-03-25T13:19:05.3917633"",""harmful"":true},{""food"":""burgers"",""notes"":""was ok"",""date"":""2024-03-28T17:21:28.4438481"",""harmful"":false},{""food"":""hotdog"",""notes"":""was ok"",""date"":""2024-03-28T17:21:37.2425487"",""harmful"":true},{""food"":""cookies"",""notes"":""was ok"",""date"":""2024-03-28T17:21:40.7761023"",""harmful"":false},{""food"":""squash"",""notes"":""was ok"",""date"":""2024-03-28T17:21:44.425102"",""harmful"":false},{""food"":""curry"",""notes"":""was ok"",""date"":""2024-03-28T17:21:48.7910556"",""harmful"":false}]";
+
+            mockHttp.When("https://localhost:")
+                .Respond("application/json", testResponse);
+
+        }
+        
 }
--- HPCFodmapProject.Server.Tests/UnitTest1.cs
+++ HPCFodmapProject.Server.Tests/UnitTest1.cs
@@ -1,16 +1,32 @@
-namespace HPCFodmapProject.Server.Tests
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+using HPCFodmapProject.Server.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HPCFodmapProject.Shared;
+using HPCFodmapProject.Server.Services;
+using HPCFodmapProject.Server.Models;
+using Moq;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+namespace HPCFodmapProject.Server.Tests;
+
+public class Tests
+{
+    private readonly Mock<IUserService> _userServiceMock = new Mock<IUserService>();
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void GetUserFoodIntake()
+    {
+        // Arrange
+        
+        var controller = new FoodIntakeController(logger.Object, _userServiceMock.Object);
+
+        // Act
+        var result = controller.GetUserFoodIntake("test");
+
+        // Assert
+        Assert.IsNotNull(result);
     }
 }
